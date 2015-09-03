1792bc1ff5b674647b0e16a0767bc9b225649914
Kevin Weber <kevin@synapsegarden.net>
2015-09-03 13:43:14 -0400

    Log-in basic elements on a DOM level

--- src/dash/views.cljs
+++ src/dash/views.cljs
@@ -7,47 +7,29 @@
   (reify om/IRender (render [_]
     (dom/div #js {:id "login-container"}
       (dom/h3 "Log-in to Dash")
-      (dom/form
-        (dom/p "Email > ")
+      (dom/form {:class "login-form"}
+        (dom/text #js {:class "login-label"} "Email")
         (dom/input #js {:type "text" :name "email"})
-        (dom/br)
-        (dom/p "Password > ")
+        (dom/text #js {:class "login-label"} "Password")
         (dom/input #js {:type "password" :name "pass"})
-        (dom/br)
         (dom/input #js {:type "submit" :value "Log-in"}))))))
+
+(defn login-table-view []
+  (reify om/IRender (render [_]
+    (dom/div #js {:id "login-container"}
+      (dom/h3 "Log-in to Dash")
+      (dom/form {:class "login-form"}
+        (dom/table {:class "login-table"}
+          (dom/tr #js {:class "login-labels"}
+            (dom/td "Email")
+            (dom/td "Password"))
+          (dom/tr #js {:class "login-elements"}
+            (dom/td (dom/input #js {:type "text" :name "email"}))
+            (dom/td (dom/input #js {:type "password" :name "pass"}))
+            (dom/td (dom/input #js {:type "submit" :value "Log-in"})))))))))
 
 (defn login-test-view []
   (reify om/IRender (render [_]
     (dom/div #js {:id "test-container"}
       (dom/h2 "Login Test View")
       (om/build login-view ())))))
-
-(defn list-view [this-list owner]
-  (reify
-    om/IRender (render [_]
-      (let [{:keys [list-name tasks]} this-list]
-        (dom/div #js {:id (str "list-" (dash-util/name-as-id list-name))}
-          (dom/h3 #js {:className "list-title"} (str list-name))
-          (apply dom/ul #js {:className "list-contents"}
-            (map
-               (fn [task] (let [{:keys [title completed date timedue]} task]
-                 (dom/li
-                   (if completed
-                     #js {:className "task-completed"}
-                     #js {:className "task-incomplete"})
-                    (str title)
-                    (dom/div #js {:className "info-date"}
-                     date)
-                    (dom/div #js {:className "info-timedue"}
-                     timedue)
-                     ))) tasks)
-        ))))))
-
-(defn lists-view [app owner]
-  (reify
-    om/IRender (render [_]
-      (dom/div #js {:id "lists-view"}
-        (dom/h2 #js {:id "lists-view_title"} (str (:user app) "'s Todo Lists"))
-        (apply dom/div #js {:className "list-view"}
-          (om/build-all list-view (:lists app))
-      )))))
