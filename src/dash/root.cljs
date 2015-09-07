8d975d8e6121b4531562eb06ae4c504cff252b16
Kevin Weber <kevin@synapsegarden.net>
2015-09-07 17:20:20 -0400

    Zeroing in on solutions

--- src/dash/root.cljs
+++ src/dash/root.cljs
@@ -6,7 +6,7 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:reload-count 0 :view [0]}))
+(defonce app-state (atom {:reload-count 0 :view 0}))
 
 (swap! app-state #(assoc % :reload-count (inc (:reload-count %))))
 
